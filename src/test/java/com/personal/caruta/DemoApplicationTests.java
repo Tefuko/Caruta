67d995fd9bc0c6beeec62506fff3e61f7cd57b18
Emi <sakura.math.sashimo@gmail.com>
2023-07-28 16:56:27 +0900

    初期フォルダ作成

---
+++ src/test/java/com/personal/caruta/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.caruta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
